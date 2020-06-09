9eaae3ea6b3b76bcb75e34eaa9f994d0a5d0bf37
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2020-06-09 11:47:39 +0300

    header_warpp

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(".js-menu__content").click(function() {
+    $('.js-menu__content').toggleClass('active');
+    // $(this).toggleClass('active');
+});
+
+$(".js-sub-menu__content").click(function() {
+    $('.js-sub-menu__content').toggleClass('active');
+    // $(this).toggleClass('active');
+});
+
+
